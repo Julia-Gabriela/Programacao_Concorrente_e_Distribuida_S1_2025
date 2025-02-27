17c4ce1fca5bc053d988b02e82bd5fe58a29d4d6
Julia-gabriela <Juliagabrielagomes09@gmail.com>
2025-02-27 09:44:55 -0300

    Aula_2_27_02_25

---
+++ Aula_2_27_02_25/EX_1.py
@@ -0,0 +1,14 @@
+import threading 
+import time 
+
+def tarefa():
+    print("Início...")
+    time.sleep(5)
+    print("Fim")
+
+    #bloco principal (main)
+
+    thread = threading.Thread(target= tarefa)
+    thread.start() # inicia a thread
+    thread.join() # Aguarda a conclusão do thread
+    print("Thread principal finalizada")
